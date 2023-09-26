60e5d55d8ed8501ccbeacc0b68fcd00eae9cbf26
RociOrellano <131103337+RociOrellano@users.noreply.github.com>
2023-09-25 22:09:02 -0300

    Commit Entregable2EF

--- Ejercicioentregable- Entidad Finanaciera/Back/Principal.cs
+++ Ejercicioentregable- Entidad Finanaciera/Back/Principal.cs
@@ -45,9 +45,63 @@
                 context.SaveChanges();
             }
         }
-        public void RealizarDeposito() { }
-        public void RealiarExtraccion() { }
-        public void RealizarTransferencia(/*string valor*/) { }
+        public void RealizarDeposito(double monto)
+        { 
+            using (var context = new ApplicationDbContext()) 
+            {
+                Cuenta_Bancaria nuevodeposito = new Cuenta_Bancaria();
+                if (monto > 0)
+                {
+                    nuevodeposito.saldo += monto;
+                }
+                else 
+                {
+                    return;//"ingrese otro monto";
+                }
+                context.SaveChanges();
+            }
+               
+        }
+        public void RealiarExtraccion(double monto) 
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                Cuenta_Bancaria nuevaextraccion = new Cuenta_Bancaria();
+                if (monto > 0 && monto <= nuevaextraccion.saldo)
+                {
+                    nuevaextraccion.saldo -= monto;
+                }
+                else if (monto > 0 )
+                {
+                    return;//"ingrese un monto mayor a cero";
+                }
+                else 
+                { 
+                    return;//"saldo insuficiente" 
+                }
+                context.SaveChanges();
+
+            }
+        }
+        public void RealizarTransferencia(Cuenta_Bancaria cuentaDestino, double monto) 
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                Cuenta_Bancaria cuentaTitular = new Cuenta_Bancaria();
+                if (monto > 0 && monto <= cuentaTitular.saldo)
+                {
+                    cuentaTitular.saldo -= monto;
+                }
+                else if (monto > 0)
+                {
+                    return;//"ingrese un monto mayor a cero";
+                }
+                else
+                {
+                    return;//"saldo insuficiente" 
+                }
+                context.SaveChanges();
+            }
         public void PagarTarjetaCredito( ) { }
         public void GenerarResumenTarjeta( ) { }
 
